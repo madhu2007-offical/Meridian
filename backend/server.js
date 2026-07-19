d6c6177d2d8b63541ab309346b594148904a8e62
madhu2007-offical <madhumithak294@gmail.com>
2026-07-19 21:56:23 +0530

    Enable trust proxy in Express server for Vercel rate-limiter support

--- backend/server.js
+++ backend/server.js
@@ -47,6 +47,7 @@ connectDB().then(() => {
 });
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(helmet());
 app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }));
